1666441e867b2a6095225f80c67414612d59b747
janaina2jsantos <janaina2jsantos@gmail.com>
2022-05-20 16:28:37 -0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,13 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Laravel</title>
-
-        <!-- <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" /> -->
         <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}" />
     </head>
-
 
     <body class="antialiased">
         <div id="app">
             <app-component></app-component>
         </div>
-
 
         <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
     </body>
